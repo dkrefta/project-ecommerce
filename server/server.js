32c6d1880edf45e1f97f9408be09cc8e61828a0a
Diego Krefta <diego.krefta@gmail.com>
2018-11-20 11:27:21 -0200

    DB adjust

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
-  console.log(`Server runing at ${port}`)
+  console.log(`Server runing at ${port}`);
 });
