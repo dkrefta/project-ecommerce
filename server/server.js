5ac8d6231dafe1c76a032fae9edcc0dcc64142e4
Diego Krefta <diego.krefta@gmail.com>
2018-11-21 10:26:14 -0200

    auth and logout

--- server/server.js
+++ server/server.js
@@ -17,7 +17,26 @@ app.use(cookieParser());
 
 const { User } = require("./models/user");
 
+//middlewares
+
+const { auth }  = require('./middleware/auth')
+
 /* USERS */
+
+app.get("/api/users/auth", auth, (req, res) => {
+  
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true, 
+    email: req.user.email, 
+    name: req.user.name,
+    lastname: req.user.name,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history
+  })
+
+})
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
@@ -25,7 +44,6 @@ app.post("/api/users/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     res.status(200).json({
       success: true,
-      userdata: doc
     });
   });
 });
@@ -50,6 +68,19 @@ app.post("/api/users/login", (req, res) => {
     });
   });
 });
+
+app.get("/api/user/logout", auth, (req, res) => {
+  User.findOneAndUpdate(
+    {_id: req.user._id},
+    {token: ''},
+    (err, doc) => {
+      if(err) return res.json({success: false, err})
+      return res.status(200).send({
+        success: true
+      })
+    }
+  )
+})
 
 const port = process.env.PORT || 3002;
 
